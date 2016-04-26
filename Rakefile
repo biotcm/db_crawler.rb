a91b4b0cb78d66dd1991d6a1fa7d7c2932f83726
Aidi Stan <aidistan@live.com>
2016-04-27 02:32:18 +0800

    Implement the main function

--- Rakefile
+++ Rakefile
@@ -3,3 +3,10 @@ task :prepare do
   system('gem install bundler -qN') if `gem list "^bundler$" -q`.chomp.empty?
   system('bundle install --quiet')
 end
+
+desc 'Run main script'
+task :run do
+  system('ruby main.rb')
+end
+
+task default: [:prepare, :run]
---
+++ main.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+require 'biotcm'
+require 'yaml'
+BioTCM.logger.level = Logger::ERROR
+
+# Make the working directory
+Dir.mkdir('crawled') unless Dir.exist?('crawled')
+
+# Load mim2gene
+if File.exist?('crawled/mim2gene.txt')
+  mim2gene = BioTCM::Table.load('crawled/mim2gene.txt')
+else
+  mim2gene = BioTCM.curl('http://omim.org/static/omim/data/mim2gene.txt').to_table
+  mim2gene.save('crawled/mim2gene.txt')
+end
+
+# Download materials
+mim2gene.row_keys.each do |mim|
+  next unless mim2gene.row(mim).first.last == 'phenotype'
+  next if File.exist?("crawled/#{mim}.yaml")
+  puts "Downloading #{mim} ..."
+
+  content = BioTCM::Databases::OMIM.new(mim).instance_variable_get(:@content)
+  File.open("crawled/#{mim}.yaml", 'w').puts YAML.dump(content)
+end
