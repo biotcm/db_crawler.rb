9fc3dd5700a65e17b6f76044d2fb660d321258d6
Aidi Stan <aidistan@live.com>
2016-05-24 00:27:15 +0800

    Add DOOR

---
+++ DOOR/README.md
@@ -0,0 +1,5 @@
+# DOOR
+
+Database of prOkaryotic OpeRons
+
+http://csbl.bmb.uga.edu/DOOR/displayspecies.php
---
+++ DOOR/main.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+require 'watir'
+require 'watir-webdriver'
+require 'net/http'
+
+# Make the working directory
+Dir.mkdir('crawled') unless Dir.exist?('crawled')
+
+# Fetch the list
+unless File.exist?('urls.txt')
+  browser = Watir::Browser.new
+  browser.goto 'http://csbl.bmb.uga.edu/DOOR/displayspecies.php'
+  browser.wait_until { browser.a(text: '1').exist? }
+  browser.select(name: 'spe_table_length').select('All')
+
+  fout = File.open('urls.txt', 'w')
+  browser.links.each do |link|
+    next unless link.text =~ /^NC/
+    fout.puts link.href
+  end
+  fout.close
+  browser.close
+end
+
+# Download directly
+urls = File.readlines('urls.txt').map(&:chomp)
+urls.each do |url|
+  /id=(?<id>\d+)/ =~ url
+  next if File.exist?("crawled/#{id}.txt")
+
+  res = Net::HTTP.get_response(URI(
+    "http://csbl.bmb.uga.edu/DOOR/downloadNCoperon.php?NC_id=#{id}"
+  ))
+
+  if res.is_a?(Net::HTTPOK)
+    File.open("crawled/#{id}.txt", 'w').puts res.body
+  end
+end
