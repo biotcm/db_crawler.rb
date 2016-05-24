4a66d34ff1b1904c86de2caf0216883df9fc0173
aidistan <aidistan@live.com>
2016-05-25 03:49:40 +0800

    Update ProOpDB crawler

--- ProOpDB/main.rb
+++ ProOpDB/main.rb
@@ -9,32 +9,38 @@ Dir.mkdir('crawled') unless Dir.exist?('crawled')
 # Start crawling
 browser = Watir::Browser.new
 browser.goto 'http://operons.ibt.unam.mx/OperonPredictor/svlByOrganism?sInitialOrg=ALL&sTypeOperon=allOperon&iOption=2'
-num_link = browser.links(class: 'enlace').size
-progressbar = ProgressBar.create(starting_at: 0, total: num_link, format: '%t: |%B| %a %e')
-(0...num_link).each do |index|
+
+unless File.exist?('scripts.txt')
+  fout = File.open('scripts.txt', 'w')
+  browser.links(class: 'enlace').map { |link| fout.puts link.href }
+  fout.close
+end
+
+scripts = File.readlines('scripts.txt').map(&:chomp)
+progressbar = ProgressBar.create(starting_at: 0, total: scripts.size, format: '%t: |%B| %a %e')
+scripts.each do |script|
   progressbar.increment
 
-  # Reset the state
-  browser.windows.first.use
-  browser.goto 'http://operons.ibt.unam.mx/OperonPredictor/svlByOrganism?sInitialOrg=ALL&sTypeOperon=allOperon&iOption=2'
-
-  # Find the link
-  link = browser.links(class: 'enlace')[index]
-  filename = link.text
-  next if File.exist?("crawled/#{filename}.txt")
+  script = script.gsub(/javascript:/, '')
+  /getOrganism\('(?<id>\w+)/ =~ script
+  next if File.exist?("crawled/#{id}.txt")
 
   # Open the link
-  link.click
+  browser.execute_script(script)
   button = browser.input(value: 'Download this data')
-  button.wait_until_present
+  button.wait_until_present(60)
   button.click
 
   # Download the entry
   popup = browser.windows.last
   popup.use
   browser.ready_state
-  File.open("crawled/#{filename}.txt", 'w').puts browser.html
+  File.open("crawled/#{id}.txt", 'w').puts browser.html
   popup.close
+
+  # Reset the state
+  browser.windows.first.use
+  browser.goto 'http://operons.ibt.unam.mx/OperonPredictor/svlByOrganism?sInitialOrg=ALL&sTypeOperon=allOperon&iOption=2'
 end
 
 # Close the browser
